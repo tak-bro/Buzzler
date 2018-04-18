20194f6bf5443805ea4bbda91de3d1285a717c1d
진형탁 <njir@naver.com>
2018-04-18 10:17:48 +0900

    feat: Add splash view

---
+++ Buzzler/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Buzzler
+//
+//  Created by 진형탁 on 2018. 4. 18..
+//  Copyright © 2018년 Maru. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
