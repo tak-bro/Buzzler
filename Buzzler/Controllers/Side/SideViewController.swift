a005bcfb51893a8b7b3a6088c79606617ac29587
진형탁 <njir@naver.com>
2018-04-29 15:45:02 +0900

    feat(side): change sideview from tableVC to VC

--- Buzzler/Controllers/Side/SideViewController.swift
+++ Buzzler/Controllers/Side/SideViewController.swift
@@ -8,30 +8,37 @@
 
 import UIKit
 
-class SideViewController: UITableViewController {
+class SideViewController: UIViewController {
 
     @IBOutlet weak var vw_header: UIView!
-    @IBOutlet weak var vw_footer: UIView!
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
         vw_header.backgroundColor = Config.UI.themeColor
-        vw_footer.backgroundColor = Config.UI.themeColor
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func pressDismiss(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pressLogout(_ sender: UIButton) {
+        dismiss(animated: false, completion: {
+            GlobalUIManager.loadLoginVC()
+        })
     }
 }
 
 extension SideViewController {
-
+/*
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NotificationCenter.default.post(name: NSNotification.Name.category, object: indexPath)
     }
+ */
 }
