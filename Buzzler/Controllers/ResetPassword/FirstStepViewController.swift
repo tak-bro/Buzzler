8c40a036d2f72dfc481a662bfe76c1b796a66382
Tak <njirtak@gmail.com>
2018-05-18 14:12:32 +0900

    feat: add FirstStepVC for resetPassword

--- Buzzler/Controllers/ResetPassword/FirstStepViewController.swift
+++ Buzzler/Controllers/ResetPassword/FirstStepViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class FirstStepViewController: UIViewController {
 
+    @IBOutlet weak var btn_next: UIButton!
+    @IBOutlet weak var txt_email: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
+}
+
+extension FirstStepViewController {
+    
+    func setUI() {
+        // remove "Back" text
+        self.navigationController?.navigationBar.topItem?.title = " "
+        
+        // button
+        btn_next.setTitleColor(Config.UI.buttonActiveColor, for: UIControlState.normal)
+        btn_next.layer.borderWidth = 2.5
+        btn_next.layer.borderColor = Config.UI.buttonActiveColor.cgColor
+        
+        // textField
+        setBorderAndCornerRadius(layer: txt_email.layer, width: 1, radius: 20, color: Config.UI.textFieldColor)
+        setLeftPadding(textField: txt_email)
+    }
 }
