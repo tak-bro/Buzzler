9e70f1dd67bff91fed9989c6b8209dfc6a579db0
Tak <njirtak@gmail.com>
2018-06-21 17:51:14 +0900

    feat(viewModel): add ManuallyEnterViewModel

--- Buzzler/ViewModels/SignUp/ManuallyEnterViewModel.swift
+++ Buzzler/ViewModels/SignUp/ManuallyEnterViewModel.swift
@@ -22,10 +22,10 @@ public protocol ManuallyEnterViewModelInputs {
 }
 
 public protocol ManuallyEnterViewModelOutputs {
-    var validatedCollegee: Driver<ValidationResult> { get }
+    var validatedCollege: Driver<ValidationResult> { get }
     var validatedMajor: Driver<ValidationResult> { get }
     var enableNextButton: Driver<Bool> { get }
-    var create: Driver<Bool> { get }
+    var createCategory: Driver<Bool> { get }
     var isLoading: Driver<Bool> { get }
     var setErrorMessage: Driver<String?> { get }
 }
@@ -33,4 +33,87 @@ public protocol ManuallyEnterViewModelOutputs {
 public protocol ManuallyEnterViewModelType {
     var inputs: ManuallyEnterViewModelInputs { get }
     var outputs: ManuallyEnterViewModelOutputs { get }
+}
+
+
+class ManuallyEnterViewModel: ManuallyEnterViewModelInputs, ManuallyEnterViewModelOutputs, ManuallyEnterViewModelType {
+    
+    // output
+    public var validatedCollege: Driver<ValidationResult>
+    public var validatedMajor: Driver<ValidationResult>
+    public var enableNextButton: Driver<Bool>
+    public var setErrorMessage: Driver<String?>
+    public var isLoading: Driver<Bool>
+    public var createCategory: Driver<Bool>
+    
+    // input
+    public var college: PublishSubject<String?>
+    public var major: PublishSubject<String?>
+    public var nextTaps: PublishSubject<Void>
+
+    public var inputs: ManuallyEnterViewModelInputs { return self }
+    public var outputs: ManuallyEnterViewModelOutputs { return self }
+    
+    // Private
+    fileprivate let provider: RxMoyaProvider<Buzzler>
+    
+    init(provider: RxMoyaProvider<Buzzler>) {
+        self.provider = provider
+        
+        self.college = PublishSubject<String?>()
+        self.major = PublishSubject<String?>()
+        self.nextTaps = PublishSubject<Void>()
+        
+        let validationService = BuzzlerDefaultValidationService.sharedValidationService
+        
+        self.validatedCollege = self.college.asDriver(onErrorJustReturn: nil)
+            .map { college in
+                return validationService.validateTextString(college!)
+        }
+        
+        self.validatedMajor = self.major.asDriver(onErrorJustReturn: nil)
+            .map { major in
+                return validationService.validateTextString(major!)
+        }
+        
+        self.enableNextButton = Driver.combineLatest(
+            validatedCollege,
+            validatedMajor) { college, major in
+                return college.isValid && major.isValid
+        }
+        
+        self.setErrorMessage = Driver.combineLatest(validatedCollege, validatedMajor) { college, major in
+            if college.isValid && major.isValid {
+                return ""
+            }
+            // return error message
+            if !college.isValid || !major.isValid {
+                return "Too short data!"
+            }
+        }
+        
+        let collegeAndMajor = Driver.combineLatest(self.college.asDriver(onErrorJustReturn: nil),
+                                                   self.major.asDriver(onErrorJustReturn: nil)) { ($0, $1) }
+
+        let isLoading = ActivityIndicator()
+        self.isLoading = isLoading.asDriver()
+        
+        self.createCategory = self.nextTaps
+            .asDriver(onErrorJustReturn:())
+            .withLatestFrom(collegeAndMajor)
+            .flatMapLatest{ data in
+                return provider.request(Buzzler.createCategory(depth: 1, name: data.0!, baseUrl: ""))
+                    .retry(3)
+                    .observeOn(MainScheduler.instance)
+                    .filterSuccessfulStatusCodes()
+                    .mapJSON()
+                    .flatMap({ res -> Single<Bool> in
+                        print("createCategory res", res)
+                        return Single.just(true)
+                    })
+                    .trackActivity(isLoading)
+                    .asDriver(onErrorJustReturn: false)
+        }
+    }
+    
 }
